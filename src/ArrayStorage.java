516ee18de836a213b964a0913d11b90a1cfba297
Serj <1177133@mail.ru>
2019-04-01 18:49:51 +0600

    BaseJava: HomeWork #2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,39 +8,49 @@ public class ArrayStorage {
     private int arraySize = 0;
     private Resume[] storage = new Resume[10000];
 
+    void update(Resume resume) {
+        int index = getIndex(resume.uuid);
+        if (index == -1) {
+            System.out.println("UPDATE ERROR: Резюме не найдено");
+        } else {
+            storage[index] = resume;
+        }
+    }
+
     void clear() {
         Arrays.fill(storage, 0, arraySize, null);
         arraySize = 0;
     }
 
     void save(Resume r) {
-        for (Resume resume : storage) {
-            if (storage[arraySize] == null) {
-                storage[arraySize] = r;
-                arraySize++;
-                break;
-            }
+        if (getIndex(r.uuid) != -1) {
+            System.out.println("SAVE ERROR: Резюме " + r.uuid + " уже существует");
+        } else if (arraySize == storage.length) {
+            System.out.println("SAVE ERROR: Массив переполнен");
+        } else {
+            storage[arraySize] = r;
+            arraySize++;
         }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < arraySize; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return storage[i];
-            }
+        if (getIndex(uuid) == -1) {
+            System.out.println("GET ERROR: Резюме не найдено");
+            return null;
         }
-        return null;
+        return storage[getIndex(uuid)];
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < arraySize; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                for (int j = i; j < arraySize; j++) {
-                    storage[j] = storage[j + 1];
-                }
-                storage[arraySize-1] = null;
-                arraySize--;
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("DELETE ERROR: Резюме не найдено");
+        } else {
+            for (int j = index; j < arraySize; j++) {
+                storage[j] = storage[j + 1];
             }
+            storage[arraySize - 1] = null;
+            arraySize--;
         }
     }
 
@@ -54,5 +64,13 @@ public class ArrayStorage {
 
     int size() {
         return arraySize;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < arraySize; i++) {
+            if (storage[i].uuid.equals(uuid))
+                return i;
+        }
+        return -1;
     }
 }
